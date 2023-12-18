7fb236651311526e6261210b72d8b6110eba2883
anthonymnf <anthonymnf30@gmail.com>
2023-12-18 16:42:15 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export default function App() {
+  const [games, setGames] = useState([]);
+  const [title, setTitle] = useState("");
+  const [cover, setCover] = useState("");
+
+  const addGame = ({ title, cover }) => {
+    const id = Math.floor(Math.random() * 100000);
+    const game = { id, title, cover };
+    setGames((state) => [...state, game]);
+  };
+
+  const removeGame = (id) => {
+    setGames((state) => state.filter((game) => game.id !== id));
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    addGame({ title, cover });
+    setCover("");
+    setTitle("");
+  };
+  return (
+    <div id="app">
+      <h1>Biblioteca de Jogos</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="cover">Capa:</label>
+          <input
+            type="text"
+            name="cover"
+            id="cover"
+            value={cover}
+            onChange={(e) => setCover(e.target.value)}
+          />
+        </div>
+        <button type="submit">Adicionar à biblioteca</button>
+      </form>
+      <div className="games">
+        {games.map((game) => (
+          <div key={game.id}>
+            <img src={game.cover} alt="Capa do Jogo" />
+            <div>
+              <h2>{game.title}</h2>
+              <button onClick={() => removeGame(game.id)}>Remover</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
