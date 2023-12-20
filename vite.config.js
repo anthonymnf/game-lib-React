fafa04d68b5f27675273615d8dfae8b65d5e6eb9
anthonymnf <anthonymnf30@gmail.com>
2023-12-20 10:25:32 -0300

    testando deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/game-lib-React",
+  base: "/game-lib-React/",
 });
